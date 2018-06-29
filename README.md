12a17589589c212790f5f3c5d9adc5a057462a5f
Greygrz <gabdu74@gmail.com>
2018-06-30 00:47:47 +0200

    fancy update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # discord-bot
+
+## Installation
+
+Put your discord bot token in `token` in the `config.json` file.
+
+Run `npm intall` to install dependancies.
+
+
--- bot.js
+++ bot.js
@@ -1,8 +1,13 @@
 var Discord = require('discord.js');
-
+var fs = require('fs');
+var fileName = './config.json';
+var config = require(fileName);
 var bot = new Discord.Client();
+var prefix = config.prefix;
+
 
 bot.on('ready', () => {
+    bot.user.setActivity(prefix + 'halp for command list')
     console.log("Logged in");
 });
 
@@ -17,83 +22,140 @@ bot.on('ready', () => {
 
 
 let dice = {
-  sides: 6,
-  roll: function () {
-    let randomNumber = Math.floor(Math.random() * this.sides) + 1;
-    return randomNumber;
-  }
+    sides: 6,
+    roll: function () {
+        let randomNumber = Math.floor(Math.random() * this.sides) + 1;
+        return randomNumber;
+    }
 }
 
 
 bot.on('message', message => {
-    if (message.content.substring(0, 1) == '!') {
+    if (message.content.substring(0, 1) == prefix) {
         let args = message.content.substring(1).split(' ');
         let cmd = args[0];
-       
+
         //args = args.splice(1);
-        switch(cmd) {
+        switch (cmd) {
             // !ping
             case 'ping':
                 message.channel.send("PONG");
-               	break;
-            // !roll
+                break;
+                // !roll
             case 'roll':
-            	//message.channel.send("roulage de dey");
-            	//
-                try{
+                //message.channel.send("roulage de dey");
+                //
+                try {
                     let request = args[1].split('+');
-                let output = "";
-                let total = 0;
+                    let output = "";
+                    let total = 0;
 
-                request.forEach(function(element) {
+                    request.forEach(function (element) {
 
-                    let test = element.split('d');
-                    let nbDices = test[0];
-                    let valueDices = test[1];
-                    
-                    if(valueDices === undefined){
-                        total += parseInt(nbDices);
-                    } else {
-                        for (let i = 0; i < nbDices; i++) {
-                            let result=Math.floor(Math.random() * valueDices) + 1;
-                            total += result;
+                        let test = element.split('d');
+                        let nbDices = test[0];
+                        let valueDices = test[1];
 
-                            output = output.concat(" "+result+" ");
+                        if (valueDices === undefined) {
+                            total += parseInt(nbDices);
+                        } else {
+                            for (let i = 0; i < nbDices; i++) {
+                                let result = Math.floor(Math.random() * valueDices) + 1;
+                                total += result;
+
+                                output = output.concat(" " + result + " ");
+                            }
+
+
                         }
-                    
+                    });
+                    /*
+                                    let nbDices = request[0];
+                                    let valueDices = request[1];
+                                    let total=0;
+                                    //message.channel.send("rolling "+nbDices+" "+valueDices+" sided dice(s)");
+                                    for (let i = 0; i < nbDices; i++) {
+                                        let result=Math.floor(Math.random() * valueDices) + 1;
+                                        total = total+result;
+                                        if(i!=0)
+                                            var output = output.concat(" + "+result);
+                                        else
+                                            var output = ""+result;
+                                    }*/
 
-                }
-                });
-/*
-                let nbDices = request[0];
-                let valueDices = request[1];
-                let total=0;
-                //message.channel.send("rolling "+nbDices+" "+valueDices+" sided dice(s)");
-                for (let i = 0; i < nbDices; i++) {
-                    let result=Math.floor(Math.random() * valueDices) + 1;
-                    total = total+result;
-                    if(i!=0)
-                        var output = output.concat(" + "+result);
-                    else
-                        var output = ""+result;
-                }*/
-
-                message.channel.send("You rolled... "+total+" ("+output+")");
-                }
-                catch(e) {
+                    message.channel.send("You rolled... " + total + " (" + output + ")");
+                } catch (e) {
                     message.channel.send("Invalid syntax; `!roll <count>d<sides>[+]<modifier>`");
                 }
-                
-            	break;
+
+                break;
             case 'maxwarp':
-            	message.channel.send("Is the best(est)");
-            	break;
+                message.channel.send("Is the best(est)");
+                break;
+            case 'halp':
+                message.channel.send({
+                    embed: {
+                        color: 0x009900,
+                        author: {
+                            name: bot.user.username,
+                            icon_url: bot.user.avatarURL
+                        },
+                        title: "COMMANDS",
+                        url: "https://github.com/Fulgryn/discord-bot",
+                        description: "prefix = `" + prefix + "`",
+                        fields: [{
+                                name: "`" + prefix + "ping`",
+                                value: "Verifies that I am online."
+      },
+                            {
+                                name: "```" + prefix + "roll <count>d<sides>[+]<modifier>```",
+                                value: "Rolls some dice."
+      },
+                            {
+                                name: "`" + prefix + "maxwarp`",
+                                value: "Saves Us All."
+      },
+                            {
+                                name: "`" + prefix + "halp`",
+                                value: "Displays just that. Yup."
+      },
+                                 {
+                                name: "`" + prefix + "prefix <new prefix>`",
+                                value: "Changes the prefix for bot commands."
+      },
+    ],
+                        timestamp: new Date(),
+                        footer: {
+                            icon_url: bot.user.avatarURL,
+                            text: "© Pomme"
+                        }
+                    }
+                });
+                break;
+            case 'prefix':
+                if (args[1].length == 1) {
+                    config.prefix = args[1];
+                    prefix=config.prefix;
+                    fs.writeFile(fileName, JSON.stringify(config, null, 2), function (err) {
+                        if (err) return console.log(err);
+                        console.log(JSON.stringify(config));
+                        console.log('writing to ' + fileName);
+                        bot.user.setActivity(prefix + 'halp for command list');
+                        message.channel.send("prefix changed to `" + prefix + "`");
+                    });
+
+                } else {
+                    message.channel.send("Your prefix is too long!");
+                }
+
+
+                break;
             default:
-				message.channel.send("This command existn't...");
-				break;
-         }
-     }
+                message.channel.send("This command existn't...");
+                break;
+        }
+    }
 });
 
 
-bot.login("Mzk2NzE0MzgzODY4MTY2MTQ1.DSliaA.cV5JZacxM859eZAQpUiRWRKHxPw");
+bot.login(config.token);
